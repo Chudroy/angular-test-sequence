a1cbb1d7f225ed6804b87696af708b1da997d619
Chudroy <nijotigajo@gmail.com>
2025-03-04 11:31:27 +0100

    Temporarily make app CSR

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'feature-song',
-    renderMode: RenderMode.Server,
-  },
+  // {
+  //   path: 'feature-song',
+  //   renderMode: RenderMode.Server,
+  // },
   {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Client,
   },
 ];
