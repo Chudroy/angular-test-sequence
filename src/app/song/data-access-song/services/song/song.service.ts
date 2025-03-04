95c3da1a5df3593c0736de4c771f1c3ac99c063d
Chudroy <nijotigajo@gmail.com>
2025-03-04 18:05:36 +0100

    Refactor song service to use _expand for artist data and improve error handling

--- src/app/song/data-access-song/services/song/song.service.ts
+++ src/app/song/data-access-song/services/song/song.service.ts
@@ -10,11 +10,33 @@ import { Song } from '../../models/song.models';
 export class SongService {
   #http = inject(HttpClient);
   readonly #API_URL = inject(API_URL);
-  readonly #SONGS_URL = this.#API_URL + '/songs?_embed=artist';
-
+  
+  readonly #SONGS_URL = this.#API_URL + '/songs?_expand=artist';
+  readonly #SONG_DETAIL_URL = (songId: string) =>
+    `${this.#API_URL}/songs/${songId}?_embed=artist`;
+  readonly DELETE_SONG_URL = (songId: string) =>
+    `${this.#API_URL}/songs/${songId}`;
+  
   getSongs(): Observable<Song[]> {
     return this.#http.get<Song[]>(this.#SONGS_URL).pipe(
-      tap((songs) => console.log(songs)),
+      catchError((error) => {
+        console.error(error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  getSong(songId: string) {
+    return this.#http.get<Song>(this.#SONG_DETAIL_URL(songId)).pipe(
+      catchError((error) => {
+        console.error(error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  deleteSong(songId: string) {
+    return this.#http.delete(this.DELETE_SONG_URL(songId)).pipe(
       catchError((error) => {
         console.error(error);
         return throwError(() => error);
