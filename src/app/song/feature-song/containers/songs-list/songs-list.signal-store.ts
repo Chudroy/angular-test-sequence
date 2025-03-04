11fe5ff2fe30ebbf0e76656b3843a775c2659d0d
Chudroy <nijotigajo@gmail.com>
2025-03-04 18:06:38 +0100

    Add song detail retrieval and deletion methods to songs store

--- src/app/song/feature-song/containers/songs-list/songs-list.signal-store.ts
+++ src/app/song/feature-song/containers/songs-list/songs-list.signal-store.ts
@@ -1,17 +1,20 @@
 import { inject } from '@angular/core';
-import { signalStore, withMethods, withState } from '@ngrx/signals';
-import { Song } from 'data-access-song';
-import { SongService } from 'src/app/song/data-access-song/services/song/song.service';
-import { patchState } from '@ngrx/signals';
-import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { pipe, switchMap, tap } from 'rxjs';
+import { Router } from '@angular/router';
 import { tapResponse } from '@ngrx/operators';
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { Song } from 'data-access-song';
+import { filter, pipe, switchMap, tap } from 'rxjs';
+import { SongService } from 'src/app/song/data-access-song/services/song/song.service';
+
 interface SongState {
+  songDetail: Song | null;
   songs: Song[];
   isLoading: boolean;
 }
 
 const initialState: SongState = {
+  songDetail: null,
   songs: [],
   isLoading: false,
 };
@@ -19,19 +22,52 @@ const initialState: SongState = {
 export const SongsStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
-  withMethods((store, songService = inject(SongService)) => ({
-    getSongs: rxMethod<void>(
-      pipe(
-        tap(() => patchState(store, { isLoading: true })),
-        switchMap(() =>
-          songService.getSongs().pipe(
-            tapResponse({
-              next: (songs) => patchState(store, { songs, isLoading: false }),
-              error: (error) => patchState(store, { isLoading: false }),
-            })
+  withMethods(
+    (store, songService = inject(SongService), router = inject(Router)) => ({
+      getSongs: rxMethod<void>(
+        pipe(
+          tap(() => patchState(store, { isLoading: true })),
+          switchMap(() =>
+            songService.getSongs().pipe(
+              tapResponse({
+                next: (songs) => patchState(store, { songs, isLoading: false }),
+                error: (error) => patchState(store, { isLoading: false }),
+              })
+            )
           )
         )
-      )
-    ),
-  }))
+      ),
+      getSongDetail: rxMethod<string | undefined>(
+        pipe(
+          filter((songId): songId is string => !!songId),
+          tap(() => patchState(store, { isLoading: true })),
+          switchMap((songId) =>
+            songService.getSong(songId).pipe(
+              tapResponse({
+                next: (song) =>
+                  patchState(store, { songDetail: song, isLoading: false }),
+                error: (error) => patchState(store, { isLoading: false }),
+              })
+            )
+          )
+        )
+      ),
+      deleteSong: rxMethod<string | undefined>(
+        pipe(
+          filter((songId): songId is string => !!songId),
+          tap(() => patchState(store, { isLoading: true })),
+          switchMap((songId) =>
+            songService.deleteSong(songId).pipe(
+              tapResponse({
+                next: () => {
+                  router.navigate(['/']);
+                },
+                error: (error) => patchState(store, { isLoading: false }),
+              })
+            )
+          )
+        )
+      ),
+    })
+  )
 );
