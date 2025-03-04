522096b975bfb41ac99633a8556be4f30414d9f4
Chudroy <nijotigajo@gmail.com>
2025-03-04 12:31:45 +0100

    Add exports for sidenav component and its signal store

--- src/app/shared/ui/index.ts
+++ src/app/shared/ui/index.ts
@@ -1,3 +1,5 @@
 export * from './header/header.component';
 export * from './header/header.signal-store';
 export * from './footer/footer/footer.component';
+export * from './side-nav/side-nav.component';
+export * from './side-nav/side-nav.signal-store';
