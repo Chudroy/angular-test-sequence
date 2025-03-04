2ebc687b7972c36ec947af8d672ede00a7456bd9
Chudroy <nijotigajo@gmail.com>
2025-03-04 09:33:05 +0100

    Add songs list component with template, styles, and unit tests

---
+++ src/app/feature-song/containers/songs-list/songs-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-songs-list',
+  imports: [],
+  templateUrl: './songs-list.component.html',
+  styleUrl: './songs-list.component.scss'
+})
+export class SongsListComponent {
+
+}
