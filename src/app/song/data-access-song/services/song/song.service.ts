7163b2cce4ee9b0ea52315c572cefc424114311c
Chudroy <nijotigajo@gmail.com>
2025-03-04 11:29:45 +0100

    Add SongService for managing song data with HTTP requests

---
+++ src/app/song/data-access-song/services/song/song.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { API_URL } from 'util-environment';
+import { Song } from '../../models/song.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SongService {
+  #http = inject(HttpClient);
+  readonly #API_URL = inject(API_URL);
+  readonly #SONGS_URL = this.#API_URL + '/songs';
+
+  getSongs(): Observable<Song[]> {
+    return this.#http.get<Song[]>(this.#SONGS_URL).pipe(
+      catchError((error) => {
+        console.error(error);
+        return throwError(() => error);
+      })
+    );
+  }
+}
