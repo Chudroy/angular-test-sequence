b9db6a6bdb14da21e4920285aae5f2c8d9eb9b83
Chudroy <nijotigajo@gmail.com>
2025-03-04 18:05:50 +0100

    Add routes for song detail and edit components

--- src/app/song/feature-song/feature-song.routes.ts
+++ src/app/song/feature-song/feature-song.routes.ts
@@ -9,6 +9,20 @@ export const featureSongRoutes: Routes = [
       ),
   },
   {
+    path: ':songId',
+    loadComponent: () =>
+      import('./containers/song-detail/song-detail.component').then(
+        (m) => m.SongDetailComponent
+      ),
+  },
+  {
+    path: ':songId/edit',
+    loadComponent: () =>
+      import('./containers/edit-song/edit-song.component').then(
+        (m) => m.EditSongComponent
+      ),
+  },
+  {
     path: 'new',
     loadComponent: () =>
       import('./containers/add-song/add-song.component').then(
