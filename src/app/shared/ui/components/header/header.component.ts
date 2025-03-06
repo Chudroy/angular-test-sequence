8293f27fd7d1a3c0cb7143003d5d75931e4216a0
Chudroy <nijotigajo@gmail.com>
2025-03-06 10:32:29 +0100

    Add language change handling to header component with translation support

--- src/app/shared/ui/components/header/header.component.ts
+++ src/app/shared/ui/components/header/header.component.ts
@@ -6,20 +6,30 @@ import {
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderStore } from './header.signal-store';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    TranslatePipe,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
   headerStore = inject(HeaderStore);
+
   toggleSidenav = output<void>();
   goBack = output<void>();
+  changeLanguage = output<string>();
 
   onToggleSidenav() {
     this.toggleSidenav.emit();
@@ -27,5 +37,10 @@ export class HeaderComponent {
 
   onGoBack() {
     this.goBack.emit();
+  }
+
+  onChangeLanguage($event: MatSelectChange<string>) {
+    const language = $event.value;
+    this.changeLanguage.emit(language);
   }
 }
