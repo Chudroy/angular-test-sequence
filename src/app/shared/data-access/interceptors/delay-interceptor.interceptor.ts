9c2fb4f1f290fcfba984688ea571cdb99f0d2958
Chudroy <nijotigajo@gmail.com>
2025-03-06 17:53:26 +0100

    Remove delay in delayInterceptorInterceptor to allow immediate request processing

--- src/app/shared/data-access/interceptors/delay-interceptor.interceptor.ts
+++ src/app/shared/data-access/interceptors/delay-interceptor.interceptor.ts
@@ -2,6 +2,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { delay } from 'rxjs';
 
 export const delayInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
-  const DELAY_TIME = 2000;
+  const DELAY_TIME = 0;
   return next(req).pipe(delay(DELAY_TIME));
 };
