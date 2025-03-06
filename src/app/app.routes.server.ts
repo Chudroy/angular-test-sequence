28ef37a7385566ad2b987805dece074ea2102417
Chudroy <nijotigajo@gmail.com>
2025-03-06 11:05:24 +0100

    Add server and client routes for song management

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,10 +1,23 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'songs',
+    renderMode: RenderMode.Server,
+  },
+  // Weird flickering issue if rendered on server
   // {
-  //   path: 'feature-song',
+  //   path: 'songs/:songId',
   //   renderMode: RenderMode.Server,
   // },
+  {
+    path: 'songs/new',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'songs/:songId/edit',
+    renderMode: RenderMode.Client,
+  },
   {
     path: '**',
     renderMode: RenderMode.Client,
