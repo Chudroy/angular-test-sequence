6cc1612e11c119ad47ccf12c5bc48df7320a071e
Chudroy <nijotigajo@gmail.com>
2025-03-06 10:31:18 +0100

    Add translation service configuration with default language support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,34 @@
 import {
   ApplicationConfig,
-  provideZoneChangeDetection,
-  provideAppInitializer,
   inject,
+  provideAppInitializer,
+  provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
-import { routes } from './app.routes';
-import { API_URL } from 'util-environment';
-import {
+  HttpClient,
   provideHttpClient,
   withFetch,
   withInterceptors,
 } from '@angular/common/http';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { API_URL } from 'util-environment';
+import { routes } from './app.routes';
+import {
   delayInterceptorInterceptor,
   PopulateStore,
 } from './shared/data-access';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
+  http: HttpClient
+) => new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -32,6 +39,14 @@ export const appConfig: ApplicationConfig = {
       withFetch(),
       withInterceptors([delayInterceptorInterceptor])
     ),
+    provideTranslateService({
+      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     provideAppInitializer(() => {
       inject(PopulateStore).fetchData();
     }),
